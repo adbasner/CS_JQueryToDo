375ec8274ce7ede1ce67091062c20e862eb385b0
Andrew Basner <andrewbasner@gmail.com>
2018-12-25 22:20:42 -0600

    add click event to ToDo item that changes style

---
+++ assets/js/todos.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function() {
+  // check off todos by clicking
+  $("li").click(function() {
+    $(this).toggleClass("completed");
+  });
+
+
+
+
+
+
+
+}); //closes document ready
+
