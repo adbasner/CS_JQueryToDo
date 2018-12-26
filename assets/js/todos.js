c4e9a0cadf7ef2d98e5e61f1dd5154543fcea2e5
Andrew Basner <andrewbasner@gmail.com>
2018-12-25 23:48:00 -0600

    finished it, oops, sorry about hte lack of commits

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,11 +1,33 @@
-
 $(document).ready(function() {
   // check off todos by clicking
-  $("li").click(function() {
+  $("ul").on("click", "li", function() {
     $(this).toggleClass("completed");
   });
 
+  // click on x to remove todo
+  $("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+      $(this).remove();
+    });
+    // prevens li click
+    event.stopPropagation();
+  });
 
+  // add input box to todo list
+  $("input[type='text']").keypress(function(event) {
+    if (event.which === 13) {
+      // get text in input
+      let newToDo = $(this).val();
+      $(this).val("");
+      // creates a new todo in the list
+      $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> " + newToDo + "</li>");
+    }
+  });
+
+  // toggle inputbox
+  $(".fa-plus").on("click", function() {
+    $("input[type='text']").fadeToggle();
+  });
 
 
 
